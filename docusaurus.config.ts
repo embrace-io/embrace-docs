df6db85b59dd81fb3536019492aa502f3e09d777
Michiel Frishert <michiel.frishert@embrace.io>
2025-04-23 12:18:42 -0700

    trigger new build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -271,7 +271,7 @@ const config: Config = {
     ],
     [
       'docusaurus-pushfeedback',{
-          project: process.env.PUSHFEEDBACK_TOKEN ?? 'missing-token',
+          project: process.env.PUSHFEEDBACK_TOKEN ?? 'missing-api-token',
           buttonPosition: 'center-right',
           modalPosition: 'sidebar-right',
           buttonStyle: 'dark',
@@ -407,7 +407,7 @@ const config: Config = {
     },
   },
   customFields: {
-    pushFeedbackToken: process.env.PUSHFEEDBACK_TOKEN ?? 'missing-token',
+    pushFeedbackToken: process.env.PUSHFEEDBACK_TOKEN ?? 'missing-api-token',
   },
 };
 
